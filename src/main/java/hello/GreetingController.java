4245bfaa1b3052d8bef919ef5d81064df30cbb88
Maxim Rogalsky <maximr@compatibl.com>
2018-05-10 17:44:34 +0300

    init

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+    
+    @MessageMapping("/hello2")
+    @SendTo("/topic/greetings2")
+    public Greeting greeting2(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
